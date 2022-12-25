e95ab31016a09307f7293b5c6ecae5494b9ff338
JMLTUnderCode <juniormiguel08062@gmail.com>
2022-12-25 15:52:15 -0400

    Modificacion y Mejora problema 506 D Concatenated Multuples

--- CodeForces/#506 D. Concatenated Multiples.cpp
+++ CodeForces/#506 D. Concatenated Multiples.cpp
@@ -2,6 +2,7 @@
 	Nombre: Junior Miguel Lara Torres
 	Carnet: 1710303
 	Nick: JMLTUnderCode
+	Problema: Codeforces Round #506 (Div. 3) D. Concatenated Multiples
 	
 	Idea General:
 	
@@ -33,68 +34,76 @@
 	
 */
 
+#include <bits/stdc++.h>
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <map>
+#include <sstream>
 
 using namespace std;
 
-int a[200005];
-map<int, long long> l_equation[20];
+// Inicializacion y definicion de variables/estructuras de datos/formatos.
+typedef map<int, long long> mill;
+typedef long long ll;
+int N, K;								// Variables basicas del problema.
+const int maximo = (2 * 1e5) + 5;		// Cota maxima para el N.
+ll aux;									// Variables de calculo auxiliar.
+ll p_contador = 0;						// Cantidad de pares que son dividibles por K.
+
+// Estructuras de datos.
+int a[maximo];
+mill l_equation[20];
+
+// Funcion para lectura de documentos/casos de prueba.
+void init_code(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+}
 
 int main() {
 	
-			// Precondicion
-	int n,  // 1 <= n <= 2*10^5
-		k;  // 2 <= k <= 10^9
-	
-	cin >> n >> k;
-	
-	// 
+	// Lectura de datos
+	init_code();
 
-	for ( int t = 1; t < n+1; t++ ) {
+	scanf("%d%d", &N, &K);
+
+	for ( int t = 1; t-1 < N; t++ ) {
 		scanf("%d", &a[t]);
 		
 		// Calculamos el lado izquierdo de la formula n1 * pow(10, len(n2)) % k 
-		long long aux = a[t];
+		aux = a[t];
 		
 		// iteramos el maximo de veces para el len posible de un numero es 10^9
 		for ( int m = 1; m <= 10; m++ ) {
-			aux = aux * 10 % k;
+			aux = aux * 10 % K;
 			
 			// Almacenamos en el mapa
 			l_equation[m][aux]++;
 		}
 	}
 	
-	long long count = 0;
-	
-	for ( int i=1; i < n+1; i++ ) {
+	for ( int i=1; i-1 < N; i++ ) {
 		// iteramos sobre todos los elementos del vector y calculamos su cantidad de digitos
 		int digits = log10(a[i]) + 1;
 		
 		//Buscamos dentro del mapa los elementos que tengan la misma cantidad de digitos que cumplan
 		// con la parte derecha de la ecuacion ( k - n2%k ) % k
-		count = count + l_equation[digits][ (k - a[i]%k)%k ];
+		p_contador += l_equation[digits][ (K - a[i]%K)%K ];
 		
 		// Para la cantidad de digitos calculamos definimos un auxiliar y calculamos el lado izquierdo de la formula
-		// dicho elemento al final del bucle permitira determina eliminar los elementos que "count" tiene de la forma
+		// dicho elemento al final del bucle permitira determina eliminar los elementos que "p_contador" tiene de la forma
 		// n1n1 divisibles por k.
 		long long aux1 = 1;
-		for ( int t = 1; t <= digits; t++ ) {
-			aux1 = aux1 * 10 % k;
-		}
-		
-		if( ( a[i] * aux1 % k + a[i] % k ) % k == 0 ) {
-			count--;
-		}
-	}
-	cout << count << endl;
+		for ( int t = 1; t <= digits; t++ ) aux1 = aux1 * 10 % K;
 	
+		if( ( a[i] * aux1 % K + a[i] % K ) % K == 0 ) p_contador--;
+	}
+
+	printf("%d\n", p_contador);
+
 	// *******************************************************************************************************************
-	// Lo comentado a continuacion es hecho por mi, pensado por mi, sudado 
-	// por mi. Pero no sirve pa CF e.e (Time limit exceeded on test 8)... Es O(n^2) T.T
+	// Lo comentado a continuacion es hecho por Junior Lara, pero se obtiene un
+	//  (Time limit exceeded on test 8)... Es O(n^2) T.T
 	/*
 			// Precondicion
 	int n,  // 1 <= n <= 2*10^5
@@ -106,9 +115,7 @@ int main() {
 		scanf("%longlong", &a[t]);
 	}
 	
-
-	
-	int count = 0;
+	int p_contador = 0;
 	unsigned long long int num1, num2;
 	
 	for ( int indx1 = 0; indx1 < n-1; indx1++ ) {
@@ -117,15 +124,15 @@ int main() {
 			// num1 es la concatenacion de (indx1)(indx2)
 			int digits2 = log10(a[indx2]) + 1;
 			num1 = a[indx1]*pow(10, digits2) + a[indx2];
-			if ( !(num1 % k != 0) ) count++;
+			if ( !(num1 % k != 0) ) p_contador++;
 			
 			// num2 es la concatenacion de (indx2)(indx1)
 			int digits1 = log10(a[indx1]) + 1;
 			num2 = a[indx2]*pow(10, digits1) + a[indx1];
-			if ( !(num2 % k != 0) ) count++;
+			if ( !(num2 % k != 0) ) p_contador++;
 	}
 	
-	cout << count << endl;
+	cout << p_contador << endl;
 	*/
 	
 	return 0;
