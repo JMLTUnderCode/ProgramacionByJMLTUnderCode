63e44c155c3906454a91100eae40f4e637f06093
JMLTUnderCode <juniormiguel08062@gmail.com>
2024-10-02 22:48:49 -0400

    Continuacion de curso TS. Inclusion de ejemplo para tipos de datos primitivos, objetcs, types, interfaces, any y arrays. Los ejemplos de explican por bloques en el script de practica p_tipoDeDatos.ts

--- TypeScript_Course/Proyecto1_Introduccion/p_tiposDeDatos.js
+++ TypeScript_Course/Proyecto1_Introduccion/p_tiposDeDatos.js
@@ -1,30 +1,120 @@
 "use strict";
-// Booleanos:
-console.log("****************** Booleanos ******************");
-let esdudiasteJS = true;
-if (esdudiasteJS) {
-    console.log('Excelente');
-}
-else {
-    console.log('Tienes que estudiarlo');
-}
-console.log("***********************************************");
-// Number
-console.log("******************  Numeros  ******************");
+// En este Script se muestra el uso de los tipos de datos en TypeScript.
+// Se crea una funcion con parametros variados y se muestra el uso de los tipos de datos.
+console.log("************************************************");
+console.log("********** Tipos de Datos: Primitivos **********\n\n");
+let barcelona = 'Barcelona';
 let goles_barca = 4;
+let real_madrid = 'Real Madrid';
 let goles_madrid = 3;
-function jugar(team1, team2) {
-    if (team1[0] > team2[0]) {
-        const str = `Gana ${team1[1]}, pierde ${team2[1]}`;
-        console.log(str);
-    }
-    else if (team1 < team2) {
-        const str = 'Gana ${team2[1]}, pierde ${team1[1]}';
-        console.log(str);
+let messi = 'Leonel Messi';
+function jugar(team1, team2, jugadorEstrella) {
+    if (team1[0] && team2[0]) { // Verificamos si esta definido.
+        let message = '';
+        if (team1[0] > team2[0])
+            message = `Gana ${team1[1]} contra ${team2[1]}`;
+        else if (team1[0] < team2[0])
+            message = `Gana ${team2[1]} contra ${team1[1]}`;
+        else
+            message = 'Empate';
+        if (jugadorEstrella) {
+            let motivo = `, porque juega ${jugadorEstrella}.`;
+            message = message + motivo;
+        }
+        console.log(message);
     }
     else {
-        console.log("Empate");
+        console.log("Falta los datos de algun equipo.");
     }
 }
-jugar([goles_barca, "Barcelona"], [goles_madrid, "Real Madrid"]);
-console.log("***********************************************");
+console.log(` __________ Marcador _________`);
+console.log(`| ${barcelona} ${goles_barca} - ${goles_madrid} ${real_madrid} |`);
+console.log(`|_____________________________|`);
+jugar([goles_barca, barcelona], [goles_madrid, real_madrid], messi);
+console.log("\n************************************************\n\n");
+console.log("************************************************");
+console.log("************** Tipos de Datos: Any *************\n\n");
+// Se usa para saltar las validaciones de tipado de TS. (No recomendado)
+let disney; // Esto es un tipo de dato any.
+disney = 'Mickey Mouse';
+console.log(disney); // Aca TS infiere que es un string.
+disney = 100;
+console.log(disney); // Aca TS infiere que es un number.
+disney = true;
+console.log(disney); // Aca TS infiere que es un boolean.
+console.log("\n************************************************\n\n");
+console.log("************************************************");
+console.log("************* Tipos de Datos: ARRAY ************\n\n");
+let etapas = [1, 2, 3, 4, 5];
+let nombres = ['Juan', 'Maria', 'Pedro'];
+console.log(etapas[1].toExponential(3));
+console.log(nombres[2].indexOf(`o`));
+console.log("\n************************************************\n\n");
+console.log("************************************************");
+console.log("************* Tipos de Datos: OBJECT ***********\n\n");
+// TS infiere los tipos de datos.
+let program = {
+    name: `Hacking`,
+    version: 1.0,
+    author: `Junior Lara`,
+    status: true,
+    script: null
+};
+// Para sobrescribir un objeto debemos escribir todos los atributos.
+program = {
+    name: `Hacking2`,
+    version: 2.0,
+    author: `Junior Lara`,
+    status: false,
+    script: null
+};
+console.log(program);
+console.log("\n************************************************\n\n");
+console.log("************************************************");
+console.log("************* Tipos de Datos: TYPES ************\n\n");
+// Ahora si podemos tipar el objeto antes creado.
+let program2 = {
+    name: `HackingPRO`,
+    version: 3.0,
+    author: `Junior Lara`,
+    status: true,
+    script: null
+};
+let junior_lara = {
+    name: `JMLTUnderCode`,
+    languages: ['JavaScript', 'TypeScript', 'Python']
+};
+let juan_andres = {
+    name: `Juan1999`,
+    age: null,
+    languages: ['JavaScript', 'TypeScript', 'Python']
+};
+console.log(junior_lara);
+console.log("\n************************************************\n\n");
+console.log("************************************************");
+console.log("*********** Tipos de Datos: INTERFACE **********\n\n");
+let developer = {
+    name: `Junior Lara`,
+    age: null,
+    languages: ['JavaScript', 'TypeScript', 'Python']
+};
+let developer2 = {
+    name: `Juan1999`,
+    age: null,
+    languages: ['JavaScript', 'TypeScript', 'Python']
+};
+// Tiene todos los atributos de la Interface con algunos adicionales.
+// Esto permite que el backend agregue mas atributos y poder seguir trabajando.
+let developer3 = {
+    name: `Carlito`,
+    lastname: `Lopez`,
+    age: null,
+    languages: ['JavaScript', 'TypeScript']
+};
+function getDeveloper(developer) {
+    console.log(`${developer.name} es un desarrollador.`);
+}
+getDeveloper(developer);
+getDeveloper(developer2);
+getDeveloper(developer3);
+console.log("\n************************************************\n\n");
--- TypeScript_Course/Proyecto1_Introduccion/p_tiposDeDatos.ts
+++ TypeScript_Course/Proyecto1_Introduccion/p_tiposDeDatos.ts
@@ -1,35 +1,172 @@
-// Booleanos:
-console.log("****************** Booleanos ******************");
+// En este Script se muestra el uso de los tipos de datos en TypeScript.
+// Se crea una funcion con parametros variados y se muestra el uso de los tipos de datos.
 
-let esdudiasteJS: boolean = true;
+console.log("************************************************");
+console.log("********** Tipos de Datos: Primitivos **********\n\n");
 
-if (esdudiasteJS) {
-    console.log('Excelente');
-} else {
-    console.log('Tienes que estudiarlo');
-}
+let barcelona : string = 'Barcelona';
+let goles_barca : number | undefined = 4;
+let real_madrid : string = 'Real Madrid';
+let goles_madrid : number | undefined = 3;
+let messi : string | null = 'Leonel Messi';
 
-console.log("***********************************************");
+function jugar(team1: [number | undefined, string], team2: [number | undefined, string], jugadorEstrella: string | null) {
+    if (team1[0] && team2[0]) { // Verificamos si esta definido.
+        let message : string = '';
+        if (team1[0] > team2[0]) message = `Gana ${team1[1]} contra ${team2[1]}`;
+        else if (team1[0] < team2[0]) message = `Gana ${team2[1]} contra ${team1[1]}`;
+        else message = 'Empate';
 
-// Number
-console.log("******************  Numeros  ******************");
+        if (jugadorEstrella){
+            let motivo : string = `, porque juega ${jugadorEstrella}.`;
+            message = message + motivo;
+        }
 
-let goles_barca : number = 4;
-let goles_madrid : number = 3;
-
-function jugar(team1: [number, string], team2: [number, string]) {
-    if (team1[0] > team2[0]) {
-        const str = `Gana ${team1[1]}, pierde ${team2[1]}`;
-        console.log(str);
-    } else if (team1 < team2) {
-        const str = 'Gana ${team2[1]}, pierde ${team1[1]}';
-        console.log(str);
+        console.log(message);
     } else {
-        console.log("Empate");
+        console.log("Falta los datos de algun equipo.");
     }
 }
+console.log(` __________ Marcador _________`);
+console.log(`| ${barcelona} ${goles_barca} - ${goles_madrid} ${real_madrid} |`);
+console.log(`|_____________________________|`);
+jugar([goles_barca, barcelona], [goles_madrid, real_madrid], messi);
 
-jugar([goles_barca, "Barcelona"], [goles_madrid, "Real Madrid"])
 
-console.log("***********************************************");
+console.log("\n************************************************\n\n");
+
+console.log("************************************************");
+console.log("************** Tipos de Datos: Any *************\n\n");
+
+// Se usa para saltar las validaciones de tipado de TS. (No recomendado)
+let disney; // Esto es un tipo de dato any.
+
+disney = 'Mickey Mouse';
+console.log(disney); // Aca TS infiere que es un string.
+
+disney = 100;
+console.log(disney); // Aca TS infiere que es un number.
+
+disney = true;
+console.log(disney); // Aca TS infiere que es un boolean.
+
+console.log("\n************************************************\n\n");
+
+console.log("************************************************");
+console.log("************* Tipos de Datos: ARRAY ************\n\n");
+
+let etapas : number[] = [1, 2, 3, 4, 5];
+
+let nombres : string[] = ['Juan', 'Maria', 'Pedro'];
+
+console.log(etapas[1].toExponential(3)) 
+console.log(nombres[2].indexOf(`o`));
+
+console.log("\n************************************************\n\n");
+
+console.log("************************************************");
+console.log("************* Tipos de Datos: OBJECT ***********\n\n");
+
+// TS infiere los tipos de datos.
+let program = {
+    name : `Hacking`,
+    version : 1.0,
+    author : `Junior Lara`,
+    status : true,
+    script : null
+}
+
+// Para sobrescribir un objeto debemos escribir todos los atributos.
+program = {
+    name : `Hacking2`,
+    version : 2.0,
+    author : `Junior Lara`,
+    status : false,
+    script : null
+}
+
+console.log(program);
+
+console.log("\n************************************************\n\n");
+
+console.log("************************************************");
+console.log("************* Tipos de Datos: TYPES ************\n\n");
+
+// Los types, interfaces y clases comienzan con mayuscula.
+type type_program = {
+    name : string,
+    version : number,
+    author : string,
+    status : boolean,
+    script : null
+}
+
+// Ahora si podemos tipar el objeto antes creado.
+let program2 : type_program = {
+    name : `HackingPRO`,
+    version : 3.0,
+    author : `Junior Lara`,
+    status : true,
+    script : null
+}
+
+// En caso de que el backend no siempre envia un dato.
+type Programmer = {
+    name : string,
+    age? : number | null,
+    languages : string[]
+}
+let junior_lara : Programmer = {
+    name : `JMLTUnderCode`,
+    languages : ['JavaScript', 'TypeScript', 'Python']
+}
+
+let juan_andres : Programmer = {
+    name : `Juan1999`,
+    age : null,
+    languages : ['JavaScript', 'TypeScript', 'Python']
+}
+
+console.log(junior_lara);
+
+console.log("\n************************************************\n\n");
+
+console.log("************************************************");
+console.log("*********** Tipos de Datos: INTERFACE **********\n\n");
+
+interface Programmer2 {
+    name : string,
+    age? : number | null,
+    languages : string[]
+}
+
+let developer : Programmer2 = {
+    name : `Junior Lara`,
+    age : null,
+    languages : ['JavaScript', 'TypeScript', 'Python']
+}
+
+let developer2 : Programmer2 = {
+    name : `Juan1999`,
+    age : null,
+    languages : ['JavaScript', 'TypeScript', 'Python']
+}
+ // Tiene todos los atributos de la Interface con algunos adicionales.
+ // Esto permite que el backend agregue mas atributos y poder seguir trabajando.
+let developer3 = {
+    name : `Carlito`,
+    lastname : `Lopez`,
+    age : null,
+    languages : ['JavaScript', 'TypeScript']
+}
+
+function getDeveloper(developer : Programmer2) {
+    console.log(`${developer.name} es un desarrollador.`);
+}
+
+getDeveloper(developer);
+getDeveloper(developer2);
+getDeveloper(developer3);
+
+console.log("\n************************************************\n\n");
 
