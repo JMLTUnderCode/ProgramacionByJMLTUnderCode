08634d7960942eaea1971478e04e4f00c2d0fde4
JMLTUnderCode <juniormiguel08062@gmail.com>
2024-10-08 15:37:57 -0400

    Modificacion final de App para manejadores de borrado e inclusion de tareas en el Manejador de Tareas.

--- TypeScript_Course/Proyecto2_React_TS/src/components/AllApps.tsx
+++ TypeScript_Course/Proyecto2_React_TS/src/components/AllApps.tsx
@@ -1,19 +1,33 @@
 import { useState } from "react";
- 
-export const AllApps = () => {
+import { ListTasks } from "./ListTasks";
 
-    const [newTasks, setNewTasks] = useState('');
+export const AllApps = () => {
+    const [newTasks, setNewTasks] = useState<string>('');
+    const [listTasks, setListTasks] = useState<string[]>([]);
+    const handleAddTask = () => {
+        if (newTasks.trim() !== '') {
+            setListTasks(lastTasks => [...lastTasks, newTasks]);
+            setNewTasks('');
+        }
+    }
+    const handleDeleteTask = (taskIndex: number) => {
+        setListTasks(currentTask => currentTask.filter((_, index) => index !== taskIndex));
+    }
+
     return (
         <div>
             <h1>Tasks Manager</h1>
             <div>
-                <input 
-                type = "text"
-                value = {newTasks}
-                onChange = {(event) => setNewTasks(event.target.value)}
-                placeholder = "New Task"
+                <input
+                    type = "text"
+                    value = {newTasks}
+                    onChange = {(event) => setNewTasks(event.target.value)}
+                    placeholder = "New Task"
                 />
+                <button style={{margin: "auto"}} onClick={handleAddTask}>Add Task</button>
+                
             </div>
+        <ListTasks listTasks={listTasks} deleteTask={handleDeleteTask}></ListTasks>
         </div>
     )
 }
