4cff5f89297c3e565a3920a4a937202aa212fa16
JMLTUnderCode <juniormiguel08062@gmail.com>
2024-10-08 15:41:27 -0400

    Arreglo y modificacion de importacion de AllApps para funcionamiento de la aplicacion.

--- TypeScript_Course/Proyecto2_React_TS/src/main.tsx
+++ TypeScript_Course/Proyecto2_React_TS/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
+import { AllApps } from './components/AllApps'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AllApps></AllApps>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<AllApps></AllApps>
+	</React.StrictMode>,
 )
